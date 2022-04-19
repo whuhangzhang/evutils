49e301d232291f57e84e291908319c44070071f0
whuhangzhang <whuhangzhang@qq.com>
2022-04-19 22:46:44 +0800

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='evutils',
+    packages=['evutils'],
+    version='0.1.0',
+    description='A series of convenience functions to make basic image processing.',
+    author='Hang Zhang',
+    keywords=['computer vision'],
+    classifiers=[],
+    scripts=[],
+)
