339631cef21699aa25a27c2b038ab6e75466c503
whuhangzhang <whuhangzhang@qq.com>
2022-04-20 00:32:24 +0800

    fix version

--- evutils/__init__.py
+++ evutils/__init__.py
@@ -1,5 +1,6 @@
 from .logger import get_root_logger, print_log
 from .json_utils import dumps, compat_dumps
+from .version import __version__
 
 __all__ = [
     'get_root_logger', 'print_log', 'dumps', 'compat_dumps'
