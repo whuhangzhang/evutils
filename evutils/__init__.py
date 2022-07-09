d5ccddbba396e0d7b6e5510cb0b40022baf8b594
whuhangzhang <whuhangzhang@qq.com>
2022-07-09 17:05:39 +0800

    add coord utils

--- evutils/__init__.py
+++ evutils/__init__.py
@@ -1,7 +1,10 @@
 from .logger import get_root_logger, print_log
 from .json_utils import dumps, compat_dumps
+from .coord_utils import gcj02_to_bd09, bd09_to_gcj02, wgs84_to_gcj02, gcj02_to_wgs84, bd09_to_wgs84, wgs84_to_bd09
+
 from .version import __version__
 
 __all__ = [
-    'get_root_logger', 'print_log', 'dumps', 'compat_dumps'
+    'get_root_logger', 'print_log', 'dumps', 'compat_dumps', 'gcj02_to_bd09', 'bd09_to_gcj02', 
+    'wgs84_to_gcj02', 'gcj02_to_wgs84', 'bd09_to_wgs84', 'wgs84_to_bd09'
 ]
