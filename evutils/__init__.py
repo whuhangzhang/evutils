9a65e74ab9ef84c36a6af13ae7776eff02dddf08
whuhangzhang <53819630+whuhangzhang@users.noreply.github.com>
2022-07-21 11:46:24 +0800

    Update __init__.py
    update init.py

--- evutils/__init__.py
+++ evutils/__init__.py
@@ -1,7 +1,8 @@
 from .logger import get_root_logger, print_log
 from .json_utils import dumps, compat_dumps
-from .coord_utils import (gcj02_to_bd09, bd09_to_gcj02, wgs84_to_gcj02, gcj02_to_wgs84, 
-                          bd09_to_wgs84, wgs84_to_bd09)
+from .coord_utils import (gcj02_to_bd09, bd09_to_gcj02, wgs84_to_gcj02, gcj02_to_wgs84, out_of_china
+                          bd09_to_wgs84, wgs84_to_bd09, wgs84_to_webMercator, webMercator_to_wgs84,
+                         gcj02_to_webMercator, webMercator_to_gcj02)
 from .colormap import colormap, random_color
 from .misc import tensor2imgs, polygon_iou, polygon_nms, img_slide_window, MyEncoder, setup_seed
 from .file_io import mkdir_p, download, recursive_walk
@@ -12,8 +13,9 @@ from .version import __version__
 
 __all__ = [
     'get_root_logger', 'print_log', 'dumps', 'compat_dumps', 'gcj02_to_bd09', 'bd09_to_gcj02', 
-    'wgs84_to_gcj02', 'gcj02_to_wgs84', 'bd09_to_wgs84', 'wgs84_to_bd09', 'colormap', 'random_color',
-    'tensor2imgs', 'polygon_iou', 'polygon_nms', 'img_slide_window', 'mkdir_p', 'download', 
+    'wgs84_to_gcj02', 'gcj02_to_wgs84', 'bd09_to_wgs84', 'wgs84_to_bd09', 'out_of_china', 
+    'wgs84_to_webMercator', 'webMercator_to_wgs84', 'gcj02_to_webMercator', 'webMercator_to_gcj02', 
+    'colormap', 'random_color', 'tensor2imgs', 'polygon_iou', 'polygon_nms', 'img_slide_window', 'mkdir_p', 'download', 
     'recursive_walk', 'load_model_weight', 'save_model', 'rename_state_dict_keys', 'fuse_conv_and_bn', 
     'fuse_model', 'replace_module', 'freeze_module', 'MyEncoder', 'sort_contours', 'label_contour', 
     'imrotate', 'imresize', 'get_rotate_crop_image', 'skeletonize', 'findContours', 'setup_seed'
