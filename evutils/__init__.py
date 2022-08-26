68cede2813c288aff37cb12e10d3cd0069419b7e
whuhangzhang <53819630+whuhangzhang@users.noreply.github.com>
2022-08-26 15:26:44 +0800

    Update __init__.py

--- evutils/__init__.py
+++ evutils/__init__.py
@@ -4,7 +4,7 @@ from .coord_utils import (gcj02_to_bd09, bd09_to_gcj02, wgs84_to_gcj02, gcj02_to
                           bd09_to_wgs84, wgs84_to_bd09, wgs84_to_webMercator, webMercator_to_wgs84,
                          gcj02_to_webMercator, webMercator_to_gcj02)
 from .colormap import colormap, random_color
-from .misc import tensor2imgs, polygon_iou, polygon_nms, img_slide_window, MyEncoder, setup_seed
+from .misc import tensor2imgs, polygon_iou, polygon_nms, MyEncoder, setup_seed
 from .file_io import mkdir_p, download, recursive_walk, read_dir, get_image_file_list
 from .torch_utils import (load_model_weight, save_model, rename_state_dict_keys, fuse_conv_and_bn, 
                           fuse_model, replace_module, freeze_module)
@@ -15,7 +15,7 @@ __all__ = [
     'get_root_logger', 'print_log', 'dumps', 'compat_dumps', 'gcj02_to_bd09', 'bd09_to_gcj02', 
     'wgs84_to_gcj02', 'gcj02_to_wgs84', 'bd09_to_wgs84', 'wgs84_to_bd09', 'out_of_china', 
     'wgs84_to_webMercator', 'webMercator_to_wgs84', 'gcj02_to_webMercator', 'webMercator_to_gcj02', 
-    'colormap', 'random_color', 'tensor2imgs', 'polygon_iou', 'polygon_nms', 'img_slide_window', 'mkdir_p', 'download', 
+    'colormap', 'random_color', 'tensor2imgs', 'polygon_iou', 'polygon_nms', 'mkdir_p', 'download', 
     'recursive_walk', 'load_model_weight', 'save_model', 'rename_state_dict_keys', 'fuse_conv_and_bn', 
     'fuse_model', 'replace_module', 'freeze_module', 'MyEncoder', 'sort_contours', 'label_contour', 
     'imrotate', 'imresize', 'get_rotate_crop_image', 'skeletonize', 'findContours', 'setup_seed', 'read_dir', 
