4e3069ab6be9083b0aa3a302c1800d8c33d4de2d
whuhangzhang <53819630+whuhangzhang@users.noreply.github.com>
2022-08-30 14:22:54 +0800

    Update __init__.py

--- evutils/__init__.py
+++ evutils/__init__.py
@@ -6,8 +6,8 @@ from .coord_utils import (gcj02_to_bd09, bd09_to_gcj02, wgs84_to_gcj02, gcj02_to
 from .colormap import colormap, random_color
 from .misc import tensor2imgs, polygon_iou, polygon_nms, MyEncoder, setup_seed
 from .file_io import mkdir_p, download, recursive_walk, read_dir, get_image_file_list
-from .torch_utils import (load_model_weight, save_model, rename_state_dict_keys, fuse_conv_and_bn, 
-                          fuse_model, replace_module, freeze_module)
+from .torch_utils import (load_model_weight, save_model, rename_state_dict_keys, copy_state_dict, 
+                          fuse_conv_and_bn, fuse_model, replace_module, freeze_module)
 from .cv2 import (findContours, imrotate, get_rotate_crop_image, skeletonize)
 from .average_meter import AverageMeter, DictAverageMeter, ListAverageMeter
 from .version import __version__
@@ -17,8 +17,8 @@ __all__ = [
     'wgs84_to_gcj02', 'gcj02_to_wgs84', 'bd09_to_wgs84', 'wgs84_to_bd09', 'out_of_china', 
     'wgs84_to_webMercator', 'webMercator_to_wgs84', 'gcj02_to_webMercator', 'webMercator_to_gcj02', 
     'colormap', 'random_color', 'tensor2imgs', 'polygon_iou', 'polygon_nms', 'mkdir_p', 'download', 
-    'recursive_walk', 'load_model_weight', 'save_model', 'rename_state_dict_keys', 'fuse_conv_and_bn', 
-    'fuse_model', 'replace_module', 'freeze_module', 'MyEncoder', 'findContours', 'imrotate',
-    'get_rotate_crop_image', 'skeletonize', 'setup_seed', 'read_dir', 'get_image_file_list',
+    'recursive_walk', 'load_model_weight', 'save_model', 'rename_state_dict_keys', 'copy_state_dict', 
+    'fuse_conv_and_bn', 'fuse_model', 'replace_module', 'freeze_module', 'MyEncoder', 'findContours', 
+    'imrotate', 'get_rotate_crop_image', 'skeletonize', 'setup_seed', 'read_dir', 'get_image_file_list',
     'AverageMeter', 'DictAverageMeter', 'ListAverageMeter'
 ]
