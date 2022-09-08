2abca4545e1845f3d56b2c90224975a2ae759491
whuhangzhang <53819630+whuhangzhang@users.noreply.github.com>
2022-09-08 17:20:33 +0800

    Update __init__.py

--- evutils/__init__.py
+++ evutils/__init__.py
@@ -10,7 +10,6 @@ from .torch_utils import (load_model_weight, save_model, rename_state_dict_keys,
                           fuse_conv_and_bn, fuse_model, replace_module, freeze_module)
 from .cv2 import (findContours, imrotate, get_rotate_crop_image, skeletonize)
 from .average_meter import AverageMeter, DictAverageMeter, ListAverageMeter, TimeAverager
-from .metrics import auc_roc, mean_iou, dice, accuracy, kappa
 from .version import __version__
 
 __all__ = [
@@ -21,6 +20,5 @@ __all__ = [
     'recursive_walk', 'load_model_weight', 'save_model', 'rename_state_dict_keys', 'copy_state_dict', 
     'fuse_conv_and_bn', 'fuse_model', 'replace_module', 'freeze_module', 'MyEncoder', 'findContours', 
     'imrotate', 'get_rotate_crop_image', 'skeletonize', 'setup_seed', 'read_dir', 'get_image_file_list',
-    'AverageMeter', 'DictAverageMeter', 'ListAverageMeter', 'TimeAverager', 'auc_roc', 'mean_iou', 
-    'dice', 'accuracy', 'kappa'
+    'AverageMeter', 'DictAverageMeter', 'ListAverageMeter', 'TimeAverager'
 ]
