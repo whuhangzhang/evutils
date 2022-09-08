2fe1e05afec3f60d76de4aac9c005914dd08bce5
whuhangzhang <53819630+whuhangzhang@users.noreply.github.com>
2022-09-08 11:47:30 +0800

    Update average_meter.py

--- evutils/average_meter.py
+++ evutils/average_meter.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 # ref: https://github.com/donnyyou/torch-segmentation/blob/75a3932b1fa676c022cc42f2ad556dc6062e79cf/lib/tools/util/average_meter.py
+import time
 
 
 class AverageMeter(object):
@@ -82,3 +83,29 @@ class ListAverageMeter(object):
 
     def info(self):
         return "[" + ', '.join(['{:.4f}'.format(x) for x in self.avg]) + "]"
+
+
+class TimeAverager(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self._cnt = 0
+        self._total_time = 0
+        self._total_samples = 0
+
+    def record(self, usetime, num_samples=None):
+        self._cnt += 1
+        self._total_time += usetime
+        if num_samples:
+            self._total_samples += num_samples
+
+    def get_average(self):
+        if self._cnt == 0:
+            return 0
+        return self._total_time / float(self._cnt)
+
+    def get_ips_average(self):
+        if not self._total_samples or self._cnt == 0:
+            return 0
+        return float(self._total_samples) / self._total_time
